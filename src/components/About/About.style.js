75ee00ddeacc469192527e72221ffe4c22abfaea
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-14 19:36:08 +0200

    responsiveness

--- src/components/About/About.style.js
+++ src/components/About/About.style.js
@@ -1,8 +1,23 @@
 import styled from "styled-components";
 
 export const StyledAbout = styled.div`
+	padding: 5rem 0;
+
+	.container {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		gap: 4rem;
+	}
+
 	.text-box {
+		flex: 1 1 100%;
 		text-align: center;
+	}
+	.advantages-box {
+		flex: 1 1 100%;
+		max-width: 450px;
+		margin: auto;
 	}
 
 	.title {
@@ -13,9 +28,31 @@ export const StyledAbout = styled.div`
 	}
 	.subtitle {
 		font-size: 0.9rem;
-		font-weight: 500;
+		font-weight: 400;
 		line-height: 2;
 		color: var(--gray-blue);
+		max-width: 550px;
+		margin: auto;
 		margin-bottom: 3rem;
+	}
+
+	@media (min-width: 1024px) {
+		.advantages-box {
+			max-width: unset;
+			flex: 1 1 40%;
+			margin: unset;
+		}
+		.text-box {
+			flex: 1 1 40%;
+			text-align: left;
+		}
+		.title {
+			font-size: 2.5rem;
+		}
+		.subtitle {
+			font-size: 1rem;
+
+			margin-right: 8rem;
+		}
 	}
 `;
