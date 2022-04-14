3e6c72c9786b7b01162c2332b588d6023b3d1ccf
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-14 19:38:42 +0200

    wrapper

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { StyledGetStarted } from "./GetStarted.style";
-import { Button } from "../Button/Button.style";
+import { Button, Wrapper } from "../../App.style";
 
 const GetStarted = () => {
 	return (
 		<StyledGetStarted>
-			<h1>Simplify how your team works today.</h1>
-			<Button white>Get Started</Button>
+			<Wrapper className="wrapper">
+				<h1>Simplify how your team works today.</h1>
+				<Button white shadow>
+					Get Started
+				</Button>
+			</Wrapper>
 		</StyledGetStarted>
 	);
 };
