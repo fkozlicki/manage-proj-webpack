b8bbf79df31e01c35ae515b1c0667003b886e798
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-14 19:31:07 +0200

    testimonials data

---
+++ src/components/Testimonials/testimonialsData.js
@@ -0,0 +1,33 @@
+// Images
+import ali from "../../assets/avatar-ali.png";
+import anisha from "../../assets/avatar-anisha.png";
+import richard from "../../assets/avatar-richard.png";
+import shanai from "../../assets/avatar-shanai.png";
+
+const data = [
+	{
+		avatar: ali,
+		name: "Ali Bravo",
+		description:
+			"“We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.”",
+	},
+	{
+		avatar: anisha,
+		name: "Anisha Li",
+		description:
+			"“Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.”",
+	},
+	{
+		avatar: richard,
+		name: "Richard Watts",
+		description:
+			"“Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!”",
+	},
+	{
+		avatar: shanai,
+		name: "Shanai Gough",
+		description:
+			"“Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.”",
+	},
+];
+export default data;
