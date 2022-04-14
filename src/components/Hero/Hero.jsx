6699f101f893e2874465b1c9f4f2cdca447d24d0
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-14 19:39:25 +0200

    wrapper + flexbox

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import heroImage from "../../assets/illustration-intro.svg";
-import { Button } from "../Button/Button.style";
+import { Button, Wrapper } from "../../App.style";
 import { StyledHero } from "./Hero.styles";
 
 const Hero = () => {
 	return (
 		<StyledHero>
-			<div>
-				<img src={heroImage} alt="charts illustration" />
-			</div>
-			<div className="text-box">
-				<h1 className="title">
-					Bring everyone together to build better products.
-				</h1>
-				<h1 className="subtitle">
-					Manage makes it simple for software teams to plan day-to-day tasks
-					while keeping the larger team goals in view.
-				</h1>
-				<Button>Get Started</Button>
-			</div>
+			<Wrapper>
+				<div className="container">
+					<div className="image-box">
+						<img src={heroImage} alt="charts illustration" />
+					</div>
+					<div className="text-box">
+						<h1 className="title">
+							Bring everyone together to build better products.
+						</h1>
+						<h1 className="subtitle">
+							Manage makes it simple for software teams to plan day-to-day tasks
+							while keeping the larger team goals in view.
+						</h1>
+						<Button shadow>Get Started</Button>
+					</div>
+				</div>
+			</Wrapper>
 		</StyledHero>
 	);
 };
