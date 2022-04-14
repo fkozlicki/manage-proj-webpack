4e70ff246f9390a4403205283d9721ff8d745f73
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-14 19:34:38 +0200

    imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Hero } from "./Hero/Hero";
 export { default as About } from "./About/About";
+export { default as Testimonials } from "./Testimonials/Testimonials";
 export { default as GetStarted } from "./GetStarted/GetStarted";
 export { default as Footer } from "./Footer/Footer";
